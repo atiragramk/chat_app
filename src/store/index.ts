f98b529766722bd62a723d49d9d71419168e662c
atiragramk <beizmargo@gmail.com>
2023-01-25 13:27:27 +0200

    [feat] started  react-redux app

---
+++ src/store/index.ts
@@ -0,0 +1,21 @@
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+
+import rootReducer from "./reducers";
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+
+export default store;
